e37603cb5989fc3825d319b8b515929e7289d196
Grey <leminemahjoub@gmail.com>
2024-02-25 04:09:14 +0100

    checker with arg

--- checker.c
+++ checker.c
@@ -6,32 +6,58 @@
 /*   By: grey <grey@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 17:31:56 by grey              #+#    #+#             */
-/*   Updated: 2024/02/25 03:38:12 by grey             ###   ########.fr       */
+/*   Updated: 2024/02/25 04:08:48 by grey             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lib/lib.h"
 #include <unistd.h>
+#include <stdlib.h>
+
+void	exit_with_error(void)
+{
+	write(1, "Error\n", 6);
+	exit(1);
+}
+
+void	process_ints_str(t_stack *a, char *str)
+{
+	while (*str)
+	{
+		while (*str == ' ')
+			str++;
+		if (*str == '\0')
+			break ;
+		push(a, char_to_int(&str));
+	}
+}
+
+void	process_ints_args(t_stack *a, char **argv)
+{
+	while (*argv)
+	{
+		if (!is_int(*argv))
+			exit_with_error();
+		push(a, char_to_int(argv));
+		argv++;
+	}
+}
 
 int	main(int argc, char *argv[])
 {
-	t_stack	pile;
-	int		value;
-	char	*temp_str;
+	t_stack	a;
 
-	temp_str = argv[argc - 1];
-	new_stack(&pile);
-	while (argc > 1)
-	{
-		if (!is_int(argv[argc - 1]))
-		{
-			write(1, "Error\n", 6);
-			return (1);
-		}
-		value = char_to_int(&temp_str);
-		push(&pile, value);
-		argc--;
-	}
-	checker(&pile);
+	new_stack(&a);
+	if (argc == 1)
+		return (0);
+	if (argc == 2)
+		process_ints_str(&a, argv[1]);
+	else if (argc > 2)
+		process_ints_args(&a, argv + 1);
+	else
+		exit_with_error();
+	if (is_sorted(&a))
+		return (0);
+	checker(&a);
 	return (0);
 }
